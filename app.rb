1edf06281e51286faf3c1b2146d0edc0af1e99cb
Haidar Muhammad Albaqir <haidar.muhammad@gmail.com>
2016-01-01 01:17:56 +0800

    fix coding style

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'instagram'
 require 'sequel'
 require 'unirest'
+require 'byebug'
 require './secret.rb'
 
 hashtags = ['ANTICORRUPTION']
-sleep_interval = 5 #seconds
+sleep_interval = 5 # seconds
 run_count = 0
 
 loop do
@@ -13,20 +14,20 @@ loop do
   client = Instagram.client(access_token: Secret.instagram['hdr'][:access_token])
 
   result_feeds = []
-  for hashtag in hashtags do
+  hashtags.each do |hashtag|
     puts "Searching for tag #{hashtag}..."
-    media_feeds = client::tag_recent_media(hashtag)
-    result_feeds = result_feeds | media_feeds
+    media_feeds = client.tag_recent_media(hashtag)
+    result_feeds |= media_feeds
     puts "Sleeping for #{sleep_interval} seconds..."
     sleep(sleep_interval)
   end
 
   # get latest timestamp
   latest_timestamp = (Unirest.get Secret.remote_url['tp'][:latest_timestamp_data]).body[0]['max_timestamp']
-  latest_timestamp = "0" if latest_timestamp.nil?
-  
-  for feed in result_feeds do
-    if feed.created_time <= latest_timestamp then
+  latest_timestamp = '0' if latest_timestamp.nil?
+
+  result_feeds.each do |feed|
+    if feed.created_time <= latest_timestamp
       puts "feed #{feed.id} is earlier #{feed.created_time} than the latest timestamp #{latest_timestamp}"
       next
     end
@@ -36,10 +37,16 @@ loop do
     message = feed.caption['text']
     timestamp = feed.created_time
     image_url = feed.images.standard_resolution.url
-    feed_hashtags = feed.tags.inject("") { |res,el| "#{res}\##{el} " }
-    #merdekas.insert(message: message, imageUrl: image_url, timeStamp: timestamp, hashtag: hashtags, instagram_id: instagram_id)
+    feed_hashtags = feed.tags.inject('') { |a, e| "#{a}\##{e} " }
+    # merdekas.insert(message: message, imageUrl: image_url, timeStamp: timestamp, hashtag: hashtags, instagram_id: instagram_id)
     response = Unirest.get Secret.remote_url['tp'][:post_data],
-                            parameters: {message: message, imageUrl: image_url, timeStamp: timestamp, hashtag: feed_hashtags, instagram_id: instagram_id}
+                           parameters: {
+                             message: message,
+                             imageUrl: image_url,
+                             timeStamp: timestamp,
+                             hashtag: feed_hashtags,
+                             instagram_id: instagram_id
+                           }
     puts response.body
   end
   run_count += 1
@@ -48,8 +55,7 @@ loop do
   sleep(sleep_interval)
 end
 
-
-#puts media_feeds
+# puts media_feeds
 #
 # domain = 'ftp.tienpingx2.96.lt'
 # username = 'u972812590'
@@ -63,5 +69,4 @@ end
 # ftp.puttextfile(path_to_web_file)
 # ftp.close
 #
-# ftp.storb
-
+# ftp.stor
